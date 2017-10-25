7fb5cc253b7cd9478b413b9b3e4968d81ec28270
Min Hu <minhu@vsp.com>
2017-10-25 14:47:29 -0700

    10/25/2017: SoldRateTest working from itellij

--- src/test/java/com/vsp/api/soldrates/test/SoldRatesTest.java
+++ src/test/java/com/vsp/api/soldrates/test/SoldRatesTest.java
@@ -17,11 +17,16 @@ public class SoldRatesTest {
     	System.out.println("process ...");
 
 		// success example
-		String clientId = "12120530";
-		String divisionId = "0021";
-		String classId = "0021";
+//		String clientId = "12120530";
+//		String divisionId = "0021";
+//		String classId = "0021";
+//		String benefitId = "A";
+//		String effDate = "20160301";
+		String clientId = "30017806";
+		String divisionId = "0001";
+		String classId = "0001";
 		String benefitId = "A";
-		String effDate = "20160301";
+		String effDate = "20170301";
 
 		String asOfDate = effDate.substring(0,4) + "-" + effDate.substring(4,6) + "-" + effDate.substring(6,8);
 		System.out.println("asOfDate=" + asOfDate);			
